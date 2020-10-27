ba69cc1e26929f1c885b3159f98fdba3c9a95b25
camshaft54 <cam.shaw1234@gmail.com>
2020-10-27 08:31:15 -0700

    Complete Person.java Add tests for Person.java in PersonClient.java

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,36 @@
 public class Person {
-    
+    String firstName;
+    String lastName;
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String first) {
+        this.firstName = first;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Person(String first, String last) {
+        firstName = first;
+        lastName = last;
+    }
+
+    public void doSomething() {
+        System.out.println("I am watching Netflix");
+    }
+
+    @Override
+    public String toString() {
+        return this.firstName + " " + this.lastName;
+    }
 }
 
 
--- src/main/java/PersonClient.java
+++ src/main/java/PersonClient.java
@@ -1,5 +1,11 @@
 public class PersonClient {
     public static void main(String[] args) {
-        
+        Person me = new Person("Cam", "Shaft");
+        me.doSomething();
+        System.out.println(me.getFirstName());
+        System.out.println(me.getLastName());
+        me.setFirstName("DirtFace");
+        me.setLastName("Dirge");
+        System.out.println(me.toString());
     }
 }
